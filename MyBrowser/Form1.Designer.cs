3aa66334894001ced24650e4e538b66974ff3f86
Diana <dianahamidullina14@gmail.com>
2023-04-13 21:09:43 +0300

    Добавьте файлы проекта.

---
+++ MyBrowser/Form1.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace MyBrowser
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.update_button = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.button4 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.tabControl1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // update_button
+            // 
+            this.update_button.Location = new System.Drawing.Point(3, 5);
+            this.update_button.Name = "update_button";
+            this.update_button.Size = new System.Drawing.Size(37, 32);
+            this.update_button.TabIndex = 0;
+            this.update_button.UseVisualStyleBackColor = true;
+            this.update_button.Click += new System.EventHandler(this.update_button_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.button4);
+            this.panel1.Controls.Add(this.button3);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.update_button);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1217, 40);
+            this.panel1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(46, 5);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(37, 32);
+            this.button1.TabIndex = 1;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(89, 5);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(37, 32);
+            this.button2.TabIndex = 2;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.Location = new System.Drawing.Point(132, 5);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(507, 28);
+            this.textBox1.TabIndex = 3;
+            // 
+            // button3
+            // 
+            this.button3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button3.Location = new System.Drawing.Point(645, 5);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(37, 32);
+            this.button3.TabIndex = 4;
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.tabControl1.Location = new System.Drawing.Point(0, 40);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(1217, 617);
+            this.tabControl1.TabIndex = 3;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Location = new System.Drawing.Point(4, 29);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(1209, 584);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Новая вкладка";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button4.Location = new System.Drawing.Point(1168, 5);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(37, 32);
+            this.button4.TabIndex = 5;
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1217, 657);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "MyBrowser";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.tabControl1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button update_button;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ MyBrowser/Form1.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CefSharp;
+using CefSharp.WinForms;
+
+namespace MyBrowser
+{
+    public partial class Form1 : Form
+    {
+        ChromiumWebBrowser chrom;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void update_button_Click(object sender, EventArgs e)
+        {
+
+            ChromiumWebBrowser chrome = tabControl1.SelectedTab.Controls[0] as ChromiumWebBrowser;
+
+            if (chrome != null && chrome.CanGoBack)
+            {
+                chrome.Back();
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            CefSettings settings = new CefSettings();
+            Cef.Initialize(settings);
+
+            chrom = new ChromiumWebBrowser("https://ya.ru"); // стартовая страница
+            chrom.AddressChanged += Chrom_AddressChanged;
+            chrom.TitleChanged += Chrom_TitleChanged;
+
+            tabControl1.SelectedTab.Controls.Add(chrom);
+        }
+
+        private void Chrom_AddressChanged(object sender, AddressChangedEventArgs e)
+        {
+            this.Invoke(new MethodInvoker(() =>
+            {
+                tabControl1.SelectedTab.Text = e.Address;
+                textBox1.Text = e.Address;
+            }));
+        
+    }
+
+        private void Chrom_TitleChanged(object sender, TitleChangedEventArgs e)
+        {
+            this.Invoke(new MethodInvoker(() =>
+            {
+                tabControl1.SelectedTab.Text = e.Title;
+               
+            }));
+        }
+
+      
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Cef.Shutdown();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            TabPage tabPage = new TabPage();
+            tabPage.Text = "Новая вкладка";
+            ChromiumWebBrowser chrome = new ChromiumWebBrowser("https://ya.ru");
+            chrom.AddressChanged += Chrom_AddressChanged;
+            chrom.TitleChanged += Chrom_TitleChanged;
+            tabPage.Controls.Add(chrome);
+            tabControl1.TabPages.Add(tabPage);
+            tabControl1.SelectTab(tabControl1.TabCount - 1);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            ChromiumWebBrowser chrome = tabControl1.SelectedTab.Controls[0] as ChromiumWebBrowser;
+
+            if (chrome != null)
+            {
+                chrome.Load(textBox1.Text);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            ChromiumWebBrowser chrome = tabControl1.SelectedTab.Controls[0] as ChromiumWebBrowser;
+
+            if(chrome != null)
+            {
+                chrome.Reload();
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            ChromiumWebBrowser chrome = tabControl1.SelectedTab.Controls[0] as ChromiumWebBrowser;
+
+            if (chrome != null  && chrome.CanGoForward)
+            {
+                chrome.Forward();
+            }
+
+
+        }
+    }
+}
